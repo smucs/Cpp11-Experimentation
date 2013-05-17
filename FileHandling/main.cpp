01a0f8ba1b66c904845b841658f719a3b0639039
Glenn L. Jenkins <glenn.l.jenkins@smu.ac.uk>
2013-05-17 22:36:40 +0100

    Needed a refresh on C++ file handling, had a play with some simple reading and writing of strings for some file parsing I'm doing for a research project.

---
+++ FileHandling/main.cpp
@@ -0,0 +1,58 @@
+//playing with C++ for input and output (text files)
+//
+//
+#include <iostream>
+#include <fstream>
+
+#include <string>
+
+using std::ofstream;
+using std::ifstream;
+using std::endl;
+using std::string;
+using std::cout;
+using std::getline;
+
+int main()
+{
+    string message = "bob";
+
+    ofstream myfile;
+
+    myfile.open("example.txt");
+
+    if(myfile.is_open())
+    {
+        myfile << "Writing this to a file." << endl;
+        myfile << message << endl;
+ 
+        myfile.close();
+    }
+
+    ifstream anotherFile;
+    message.erase(message.begin(),message.end());
+
+    anotherFile.open("example.txt");
+
+//   anotherFile >> message;
+//   cout << message;
+
+//   anotherFile >> message;
+//   cout  << message;
+
+    if(anotherFile.is_open())
+    {
+        while(anotherFile.good())
+        {
+            getline(anotherFile,message);
+            cout << message << endl;
+        }
+        anotherFile.close();
+    }
+
+    anotherFile.close();
+
+   return 0;
+}
+
+
